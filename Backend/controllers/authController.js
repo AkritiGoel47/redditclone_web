ee119c53570ac4bee5ca1a077e572f4bc9373243
AkritiGoel47 <goel.akriti11082gmail.com>
2024-08-03 14:43:17 +0530

    cors issue

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -48,13 +48,13 @@ const registerUser = async (req, res) => {
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV, 
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 2 * 60 * 60 * 1000, // 2 hours
     });
     res.cookie("user_id", userData._id.toString(), {
       httpOnly: true,
       secure: process.env.NODE_ENV, 
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 2 * 60 * 60 * 1000, // 2 hours
     });
 
@@ -106,13 +106,13 @@ const loginUser = async (req, res) => {
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV, 
-sameSite: 'strict',
+sameSite: 'none',
       maxAge: 2 * 60 * 60 * 1000, // 2 hours
     });
     res.cookie("user_id", userData._id.toString(), {
       httpOnly: true,
       secure: process.env.NODE_ENV, 
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 2 * 60 * 60 * 1000, // 2 hours
     });
 
